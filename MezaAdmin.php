597ca5e7a2e760ea566921bfb927f2135bb86556
Kris Field <kristopher.field@gmail.com>
2018-10-28 20:28:25 -0500

    fix typos

--- MezaAdmin.php
+++ MezaAdmin.php
@@ -1,10 +1,10 @@
 <?php
 
 if ( function_exists( 'wfLoadExtension' ) ) {
-	wfLoadExtension( 'MyFirstExtension' );
+	wfLoadExtension( 'MezaAdmin' );
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['MezaAdmin'] = __DIR__ . '/i18n';
-	$wgExtensionMessagesFiles['MezaAdmin'] = __DIR__ . '/MezaAdmin.i18n.alias.php';
+	$wgExtensionMessagesFiles['MezaAdminAlias'] = __DIR__ . '/MezaAdmin.i18n.alias.php';
 	$wgExtensionMessagesFiles['MezaAdminMagic'] = __DIR__ . '/MezaAdmin.i18n.magic.php';
 	wfWarn(
 		'Deprecated PHP entry point used for MezaAdmin extension. Please use wfLoadExtension ' .
